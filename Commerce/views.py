81a2b571462192d51bfc39dd7f90b5c122befd7b
Sadath136 <76907780+Sadath136@users.noreply.github.com>
2021-10-18 12:59:06 +0530

    Update views.py

--- Commerce/views.py
+++ Commerce/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import boto3
-from models import Collect
+from . import models
 def homepage(request):
     information = Collect.objects.all()
     context = {'info': information}
