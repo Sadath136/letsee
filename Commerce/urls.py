3fa8c801912a63331cc4646163e0649b62da5016
Sadath136 <76907780+Sadath136@users.noreply.github.com>
2021-10-18 10:07:58 +0530

    Update urls.py

--- Commerce/urls.py
+++ Commerce/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.http import HttpResponse
-from .views import homepage,su
+from .views import homepage,su,admin_site
 urlpatterns=[
     path('',homepage),
-    path('uploaded sucessfully',su,name='home')
+    path('uploaded sucessfully',su,name='home'),
+    path('admin_area',admin_site,name='admin_site')
 ]
