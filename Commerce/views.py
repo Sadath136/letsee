4d6d0bbab138e254bc7bf620c54d3faa7605c544
Sadath136 <76907780+Sadath136@users.noreply.github.com>
2021-10-18 09:27:35 +0530

    Update views.py

--- Commerce/views.py
+++ Commerce/views.py
@@ -12,18 +12,27 @@ def su(request):
         db = mysql.connector.connect(host="deployment.clm4ibgvdrzu.us-east-2.rds.amazonaws.com", passwd="Sadath8151",
                                      user="Sadath", database="Deployment")
         cursor = db.cursor()
-        cursor.execute("insert into Test_collect(name,phone_number,image)values(%s,%s,%s)",
-                       (name, phonenumber, file.name))
-        db.commit()
-        db.close()
-        import mysql.connector
-        db = mysql.connector.connect(host="deployment.clm4ibgvdrzu.us-east-2.rds.amazonaws.com", passwd="Sadath8151",
-                                     user="Sadath", database="Deployment")
-        cursor = db.cursor()
-        cursor.execute('select id from Test_collect where phone_number=%s and name=%s',(phonenumber,name))
-        kp=cursor.fetchone()
-        s3=boto3.client('s3',aws_access_key_id="AKIA5UUHTIOD2M724W6J",aws_secret_access_key="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")
-        s3.put_object(Body=file,Key=str(*kp)+str(file.name),Bucket="form-deployment")
-        db.close()
-        return render(request,'Commerce/suc.html')
-
+        cursor.execute('select * from Test_collect where phone_number=%s',(phonenumber,))
+        details=cursor.fetchall()
+        if details==[]:
+            import mysql.connector
+            db = mysql.connector.connect(host="deployment.clm4ibgvdrzu.us-east-2.rds.amazonaws.com", passwd="Sadath8151",
+                                         user="Sadath", database="Deployment")
+            cursor = db.cursor()
+            cursor.execute("insert into Test_collect(name,phone_number,image)values(%s,%s,%s)",
+                           (name, phonenumber, file.name))
+            db.commit()
+            db.close()
+            import mysql.connector
+            db = mysql.connector.connect(host="deployment.clm4ibgvdrzu.us-east-2.rds.amazonaws.com", passwd="Sadath8151",
+                                         user="Sadath", database="Deployment")
+            cursor = db.cursor()
+            cursor.execute('select id from Test_collect where phone_number=%s and name=%s',(phonenumber,name))
+            kp=cursor.fetchone()
+            s3=boto3.client('s3',aws_access_key_id="AKIA5UUHTIOD2M724W6J",aws_secret_access_key="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")
+            s3.put_object(Body=file,Key=str(*kp)+str(file.name),Bucket="form-deployment")
+            db.close()
+            return render(request,'Commerce/suc.html')
+        else:
+            return HttpResponse("<h1><i>this person has already been registered</i></h1>")
+                    
