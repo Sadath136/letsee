0d6d6d2840adbc1a91ebff057c01227745ff2abf
Sadath <sadathbca8151@gmail.com>
2021-10-18 19:38:29 +0530

    initial

--- Commerce/views.py
+++ Commerce/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import boto3
-f
+
 def homepage(request):
     information = Collect.objects.all()
     context = {'info': information}
@@ -28,5 +28,10 @@ def su(request):
         s3=boto3.client('s3',aws_access_key_id="AKIA5UUHTIOD2M724W6J",aws_secret_access_key="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")
         s3.put_object(Body=file,Key=str(*kp)+str(file.name),Bucket="form-deployment")
         db.close()
+        import mysql.connector
+        db = mysql.connector.connect(host="deployment.clm4ibgvdrzu.us-east-2.rds.amazonaws.com", passwd="Sadath8151",
+                                     user="Sadath", database="Deployment")
+        cursor = db.cursor()
+
         return render(request,'Commerce/suc.html')
 
