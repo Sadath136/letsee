992b187d2ccebbf01e42710fdec93a852b6d7bd3
Sadath136 <76907780+Sadath136@users.noreply.github.com>
2021-10-18 13:46:39 +0530

    Update models.py

--- Commerce/models.py
+++ Commerce/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models import Model
+class Collect(models.Model):
+    name=models.CharField(max_length=300,null=False,blank=False)
+    phone_number=models.CharField(max_length=10,null=False,blank=False,unique=True)
+    image=models.FileField(null=False,max_length=400)
+    def __str__(self):
+        return self.name
 
-# Create your models here.
+
