45cc3f05dceb13e105e854e4adec99ac0d743819
Sadath <sadathbca8151@gmail.com>
2021-10-26 22:17:33 +0530

    letsee

--- Commerce/views.py
+++ Commerce/views.py
@@ -3,17 +3,7 @@ from django.http import HttpResponse
 import boto3
 from .models import Collect
 def homepage(request):
-    import mysql.connector
-
-    db = mysql.connector.connect(host="deployment.clm4ibgvdrzu.us-east-2.rds.amazonaws.com", passwd="Sadath8151",
-                                 user="Sadath", database="Deployment")
-    cursor = db.cursor()
-    cursor.execute('select * from Commerce_collect')
-    p = cursor.fetchall()
-    db.commit()
-    db.close()
-    context={'list':p}
-    return render(request,'Commerce/homepage.html',context)
+    return render(request,'Commerce/homepage.html')
 def su(request):
     if request.method=="POST":
         name = request.POST.get('username')
@@ -43,6 +33,17 @@ def su(request):
             s3=boto3.client('s3',aws_access_key_id="AKIA5UUHTIOD2M724W6J",aws_secret_access_key="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")
             s3.put_object(Body=file,Key=str(*kp)+str(file.name),Bucket="form-deployment")
             db.close()
-            return render(request,'Commerce/suc.html')
+            import mysql.connector
+
+            db = mysql.connector.connect(host="deployment.clm4ibgvdrzu.us-east-2.rds.amazonaws.com",
+                                         passwd="Sadath8151",
+                                         user="Sadath", database="Deployment")
+            cursor = db.cursor()
+            cursor.execute('select * from Commerce_collect')
+            p = cursor.fetchall()
+            db.commit()
+            db.close()
+            context = {'list': p}
+            return render(request,'Commerce/suc.html',context)
         else:
             return HttpResponse("<h1><em>You have already registered your phone number</em></h1>")
