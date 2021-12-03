dbd305b433e683e66af6093fb7d5d24480a8ea10
Sadath136 <76907780+Sadath136@users.noreply.github.com>
2021-12-03 19:07:28 +0530

    Update admin.py

--- Commerce/admin.py
+++ Commerce/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from .models import Collect
-class CollectAdmin(admin.ModelAdmin):
-    fields = ['name','phone_number','image']
-    search_fields = ['name','phone_number','image']
-    list_filter = ['name','phone_number','image']
-admin.site.register(Collect,CollectAdmin)
